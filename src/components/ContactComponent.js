633cee9244be9b3ca785a5e24f7022886da8a0a7
Harshit Sharma <harshitsharmabtp@gmail.com>
2020-10-14 20:10:56 +0530

    fetch from server

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Breadcrumb,BreadcrumbItem, Button,Label,Col,Row} from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors ,actions} from 'react-redux-form';
 import {Link} from 'react-router-dom';
 
 const required = (val) => val && val.length;
@@ -21,6 +21,7 @@ class Contact extends Component {
   handleSubmit(values) {
       console.log("Current state is: "+JSON.stringify(values));
       alert("Current state is: "+JSON.stringify(values));
+      this.props.resetFeedbackForm();
       // event.preventDefault();
   }
 
@@ -73,7 +74,7 @@ class Contact extends Component {
                        <h3>Send Us Your Feedback</h3>
                    </div>
                    <div className="col-12 col-md-9">
-                   <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                   <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                    <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -197,7 +198,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                    </div>
                 </div>
             </div>
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card,CardImg,CardText,CardBody,CardTitle,Modal,ModalHeader,ModalBody,Br
 import { Control, LocalForm, Errors } from "react-redux-form";
 import {Loading} from './LoadingComponent';
 import { Link } from "react-router-dom";
+import {baseUrl} from '../shared/baseUrl';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -119,7 +120,7 @@ function RenderDish({dish}) {
   return (
     <div className="col-12 col-md-5 m-1">
         <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
+          <CardImg top src={baseUrl + dish.image} alt={dish.name} />
           <CardBody>
             <CardTitle>{dish.name}</CardTitle>
             <CardText>{dish.description}</CardText>
