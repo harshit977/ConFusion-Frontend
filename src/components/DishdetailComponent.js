0fb7f8b53adbad300fd05d3f7668df42ce1dc9d1
Harshit Sharma <harshitsharmabtp@gmail.com>
2020-10-15 11:55:22 +0530

    error handling , post comments and animations

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Control, LocalForm, Errors } from "react-redux-form";
 import {Loading} from './LoadingComponent';
 import { Link } from "react-router-dom";
 import {baseUrl} from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -31,7 +32,7 @@ class CommentForm extends Component {
   handleSubmit(values) 
   {
     this.toggleModal();
-    this.props.addComment(this.props.dishId,values.rating, values.author, values.comment);
+    this.props.postComment(this.props.dishId,values.rating, values.author, values.comment);
   }
 
   render() {
@@ -119,6 +120,11 @@ class CommentForm extends Component {
 function RenderDish({dish}) {
   return (
     <div className="col-12 col-md-5 m-1">
+      <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
         <Card>
           <CardImg top src={baseUrl + dish.image} alt={dish.name} />
           <CardBody>
@@ -126,18 +132,21 @@ function RenderDish({dish}) {
             <CardText>{dish.description}</CardText>
           </CardBody>
         </Card>
+        </FadeTransform>
     </div>
   );
 }
 
-function RenderComments({ comments, dishId , addComment}) {
+function RenderComments({ comments, dishId , postComment}) {
   if (comments != null) {
     return (
       <div className="col-12 col-md-5 m-1">
         <h4>Comments</h4>
         <ul className="list-unstyled">
+        <Stagger in>
           {comments.map(comment => {
             return (
+              <Fade in>
                 <li key={comment.id}>
                   <p>{comment.comment}</p>
                   <p>
@@ -149,10 +158,12 @@ function RenderComments({ comments, dishId , addComment}) {
                     }).format(new Date(Date.parse(comment.date)))}
                   </p>
                 </li>
+                </Fade>
             );
           })}
+          </Stagger>
           </ul>
-        <CommentForm dishId={dishId} addComment={addComment}/>
+        <CommentForm dishId={dishId} postComment={postComment}/>
       </div>
     );
   } else return <div />;
@@ -198,7 +209,7 @@ const DishDetailComponent = props =>
           <RenderComments
             comments={props.comments}
             dishId={props.dish.id}
-            addComment={props.addComment}
+            postComment={props.postComment}
           />
         </div>
       </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,8 +8,9 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {Switch , Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment , fetchDishes ,fetchComments,fetchPromos} from '../redux/ActionCreators';
+import { postComment , fetchDishes ,fetchComments,fetchPromos} from '../redux/ActionCreators';
 import {actions} from 'react-redux-form';
+import { TransitionGroup , CSSTransition } from 'react-transition-group';
 
 const mapStateToProps = state => {
     return {
@@ -21,7 +22,7 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  addComment: (dishId,rating,author,comment) => dispatch(addComment(dishId,rating,author,comment)),
+  postComment: (dishId,rating,author,comment) => dispatch(postComment(dishId,rating,author,comment)),
   fetchDishes: () => {dispatch(fetchDishes())},
   resetFeedbackForm: () => {dispatch(actions.reset('feedback'))},
   fetchComments: () => {dispatch(fetchComments())},
@@ -63,7 +64,7 @@ class Main extends Component{
             errMess={this.props.dishes.errMess}
             comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} 
             errMess={this.props.comments.errMess}
-            addComment={this.props.addComment}
+            postComment={this.props.postComment}
             />
       );
     };
@@ -71,7 +72,8 @@ class Main extends Component{
     return (
       <div>
         <Header />
-        <div>
+        <TransitionGroup>
+          <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
           <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/aboutus' component={() => <Aboutus leaders={this.props.leaders} />} />} />
@@ -80,7 +82,8 @@ class Main extends Component{
               <Route exact path='/contactus' component={() =>  <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
               <Redirect to="/home" />
           </Switch>
-        </div>
+          </CSSTransition>
+        </TransitionGroup>
         <Footer />
       </div>
     );
